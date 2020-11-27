b7f29826ca5c72d76fc6f9e1524397e196814b4d
loaishar <55962133+loaishar@users.noreply.github.com>
2020-11-27 13:47:33 +0200

    commit

--- work.java
+++ work.java
@@ -1,23 +1,41 @@
 import java.util.Scanner;
+
 public class work {
-       public static void main(String[] args) {
+
+       public static String poly(double[] poly) {
               Scanner sc = new Scanner(System.in);
               char x = sc.next().charAt(0);
               String s = Character.toString(x);
               String ans = "";
-              double[] arr = { 2,0,3,-1};
+              double[] arr = { 2, 0, 3, -1 };
               for (int i = arr.length - 1; i >= 0; i--) {
-                     if (arr[i] != 0 && i!=0) {
-                            ans += arr[i]+s+"^"+i+"+";
+                     if (arr[i] != 0 && i != 0) {
+                            ans += arr[i] + s + "^" + i + "+";
                      }
-                     if (arr[i] != 0 && i==0) {
-                            ans+=arr[i];
+                     if (arr[i] != 0 && i == 0) {
+                            ans += arr[i];
                      }
               }
-              int y= ans.length()-1;
-              if(ans.charAt(y)=='+')
-              ans =ans.substring(0,y);
+              int y = ans.length() - 1;
+              if (ans.charAt(y) == '+')
+                     ans = ans.substring(0, y)
 
-              System.out.println(ans);
+              return ans;
+
        }
+
+       
+
+       public static double root (double[] p,double x1,double x2,double eps){
+
+}
+
+
+
+
+
+
+
+public static void main(String[] args) {
+
 }
