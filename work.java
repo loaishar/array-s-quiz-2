770187276ac4804adde2e862ec2e1aed7ada85d5
loaishar <55962133+loaishar@users.noreply.github.com>
2020-11-27 02:18:41 +0200

    commit on master

--- work.java
+++ work.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
 public class work {
-
        public static void main(String[] args) {
-              String ans = " ";
+              Scanner sc = new Scanner(System.in);
+              char x = sc.next().charAt(0);
+              String s = Character.toString(x);
+              String ans = "";
               double[] arr = { 2,0,3,-1};
               for (int i = arr.length - 1; i >= 0; i--) {
                      if (arr[i] != 0 && i!=0) {
-                            ans += arr[i]+"x"+"^"+i+"+";
+                            ans += arr[i]+s+"^"+i+"+";
                      }
                      if (arr[i] != 0 && i==0) {
                             ans+=arr[i];
